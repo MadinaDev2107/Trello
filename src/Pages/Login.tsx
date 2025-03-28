aa76637376d1d08bcb2b69a59f353ab4715b7cd1
MadinaDev2107 <muzaffarovna2103@gmail.com>
2025-03-28 21:23:21 +0500

    changed

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -52,7 +52,7 @@ const Login = () => {
   }
 
   return (
-    <div className="d-flex justify-content-center align-items-start vh-100 bg-primary">
+    <div className="d-flex justify-content-center align-items-start vh-100 bg-dark">
       <div className="card p-3 w-100 text-center m-2" style={{ maxWidth: "400px" }}>
         <div className="border-bottom border-2 mb-2">
           <h2>Login</h2>
--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -47,7 +47,7 @@ const Register = () => {
   }
 
   return (
-    <div className="d-flex justify-content-center align-items-start  vh-100 bg-primary">
+    <div className="d-flex justify-content-center align-items-start  vh-100 bg-dark">
       <div
         className="card p-3 w-100 text-center m-2"
         style={{ maxWidth: "400px" }}
